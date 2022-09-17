05baf5701afd63da5d07e1f2ea89cf16b545a2bc
“s-mastrom” <60471784+smastrom1@users.noreply.github.com>
2022-09-17 13:38:01 +0200

    add ref, basic behavior

--- src/Email.tsx
+++ src/Email.tsx
@@ -1,156 +1,235 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { forwardRef, useEffect, useMemo, useRef, useState } from 'react';
 
 type Maybe<T> = T | undefined;
 
 type Attributes = {
+	id?: string;
 	placeholder?: string;
 	minLength?: number;
 };
 
-export type Domain = string; // `${string}.${string}`;
+type CustomClasses = {
+	wrapperClassName?: string;
+	inputClassName?: string;
+	dropdownClassName?: string;
+	suggestionClassName?: string;
+	usernameClassName?: string;
+	domainClassName?: string;
+};
 
 type Props = {
 	value: Maybe<string>;
 	onChange: (value: string) => void;
-	baseList: Domain[];
-	domainList: Domain[];
-	defaultValue: string;
+	baseList: string[];
+	domainList?: string[];
+	defaultValue?: string;
+	animation?: string;
+	classNames?: CustomClasses;
+	scrollIntoView?: boolean;
+	closeOnScroll?: boolean;
 };
 
-function clean(value: string) {
-	return value.replace(/\s+/g, '');
+function cleanValue(value: string) {
+	return value.replace(/\s+/g, '').toLowerCase();
 }
 
 function isValidArr(maybeArr: unknown) {
 	return Array.isArray(maybeArr) && maybeArr.length > 0;
 }
+
 function isInvalid(value: unknown) {
 	return typeof value !== 'string';
 }
 
-export function Email({
-	value: externalValue,
-	defaultValue,
-	onChange,
-	placeholder,
-	minLength,
-	baseList,
-	domainList = [],
-}: Attributes & Props) {
-	/* Data */
+export const Email = forwardRef<HTMLInputElement, Attributes & Props>(
+	(
+		{
+			id,
+			value: externalValue,
+			defaultValue,
+			onChange,
+			placeholder,
+			minLength,
+			baseList,
+			domainList = [],
+			animation,
+			classNames,
+		},
+		externalRef
+	) => {
+		/* Data */
 
-	const internalDomainList = useMemo(() => domainList, [domainList]);
+		const internalDomainList = useMemo(() => domainList, [domainList]);
 
-	/* Refs */
+		/* Refs */
 
-	const isSelected = useRef(false);
-	const internalRef = useRef<HTMLInputElement>(null);
-	const isInitial = useRef(true);
+		const internalRef = useRef<HTMLInputElement | null>(null);
+		const wrapperRef = useRef<HTMLDivElement | null>(null);
 
-	/* State */
+		const isSelected = useRef(false);
+		const isInitial = useRef(true);
 
-	const [internalDomains, setInternalDomains] = useState(baseList);
-	const [isOpen, setIsOpen] = useState(false);
+		/* State */
 
-	/* Data Helpers */
+		const [internalDomains, setInternalDomains] = useState(baseList);
+		const [isOpen, setIsOpen] = useState(false);
 
-	const absoluteValue = isInvalid(externalValue)
-		? ''
-		: clean(externalValue as string).toLowerCase();
+		/* Data Helpers */
 
-	const splitVal = absoluteValue.split('@');
+		const absoluteValue = isInvalid(externalValue) ? '' : cleanValue(externalValue as string);
+		const splitVal = absoluteValue.split('@');
 
-	const username = splitVal[0];
-	const hasUsername = username.length >= 1;
-	const hasAt = hasUsername && absoluteValue.indexOf('@') !== -1;
-	const domain = splitVal[1];
-	const hasDomain = hasAt && domain.length >= 1;
+		const username = splitVal[0];
+		const hasUsername = username.length >= 1;
+		const hasAt = hasUsername && absoluteValue.indexOf('@') !== -1;
+		const domain = splitVal[1];
+		const hasDomain = hasAt && domain.length >= 1;
 
-	/* UI Helpers */
+		/* UI Helpers */
 
-	const wantsRefine = isValidArr(internalDomainList);
-	const shouldRefine = hasDomain && wantsRefine;
+		const wantsRefine = isValidArr(internalDomainList);
+		const shouldRefine = hasDomain && wantsRefine;
 
-	/* Effects */
+		/* Effects */
 
-	useEffect(() => {
-		if (isInitial.current) {
-			isInitial.current = false;
-		} else if (isSelected.current) {
-			isSelected.current = false;
-		} else {
-			if (hasUsername) {
-				setIsOpen(true);
+		useEffect(() => {
+			if (isInitial.current) {
+				isInitial.current = false;
+			} else if (isSelected.current) {
+				isSelected.current = false;
+			} else {
+				if (hasUsername) {
+					setIsOpen(true);
 
-				if (shouldRefine) {
-					const copyDomains = [...internalDomainList];
-					const newDomains = copyDomains
-						.filter((userDomain) => userDomain.startsWith(domain))
-						.slice(0, 6);
+					if (shouldRefine) {
+						const copyDomains = [...internalDomainList];
+						const newDomains = copyDomains
+							.filter((userDomain) => userDomain.startsWith(domain))
+							.slice(0, 6);
 
-					if (newDomains.length > 0) {
-						setInternalDomains(newDomains);
+						if (newDomains.length > 0) {
+							setInternalDomains(newDomains);
+						} else {
+							setIsOpen(false);
+						}
 					} else {
-						setIsOpen(false);
+						setInternalDomains(baseList);
 					}
 				} else {
-					setInternalDomains(baseList);
+					setIsOpen(false);
 				}
-			} else {
+			}
+		}, [externalValue, domain, hasUsername, hasAt, shouldRefine, internalDomainList, baseList]);
+
+		useEffect(() => {
+			function handleClose(event: MouseEvent) {
+				if (!wrapperRef.current?.contains(event.target as Node)) {
+					setIsOpen(false);
+				}
+			}
+			if (isOpen) {
+				document.addEventListener('click', handleClose);
+
+				return () => {
+					document.removeEventListener('click', handleClose);
+				};
+			}
+		}, [isOpen]);
+
+		/* Handlers */
+
+		function handleInternalChange(event: React.ChangeEvent<HTMLInputElement>) {
+			onChange(cleanValue(event.target.value));
+		}
+
+		function handleReFocus() {
+			if (internalRef.current !== null) {
+				internalRef.current.focus();
+			}
+		}
+
+		function handleSuggestionClick(event: React.MouseEvent<HTMLLIElement>) {
+			event.preventDefault();
+			event.stopPropagation();
+			{
+				onChange(cleanValue((event.currentTarget as Node).textContent as string));
+				handleReFocus();
+				isSelected.current = true;
 				setIsOpen(false);
 			}
 		}
-	}, [externalValue, domain, hasUsername, hasAt, shouldRefine, internalDomainList, baseList]);
 
-	/* Handlers */
+		/* User Attributes */
 
-	function handleInternalChange(event: React.ChangeEvent<HTMLInputElement>) {
-		onChange(clean(event.target.value));
-	}
+		const userAttrs: Attributes = {
+			id,
+			placeholder,
+			minLength,
+		};
 
-	function handleReFocus() {
-		if (internalRef.current !== null) {
-			internalRef.current.focus();
+		function getListStyles() {
+			const display = { display: isOpen ? 'inherit' : 'none' };
+
+			if (animation) {
+				return {
+					style: {
+						animation,
+						...display,
+					},
+				};
+			}
+			return { style: display };
 		}
-	}
 
-	function handleSuggestionClick(event: React.MouseEvent<HTMLLIElement>) {
-		event.preventDefault();
-		event.stopPropagation();
-		{
-			onChange(clean((event.target as Node).textContent as string));
-			handleReFocus();
-			isSelected.current = true;
-			setIsOpen(false);
+		function getClasses(classProperty: keyof CustomClasses) {
+			if (typeof classNames !== 'undefined' && typeof classNames[classProperty] === 'string') {
+				return { className: classNames[classProperty] };
+			}
+			return {};
 		}
-	}
 
-	/* User Attributes */
+		/* Prevent rendering */
 
-	const userAttrs: Attributes = {
-		placeholder,
-		minLength,
-	};
+		if (!baseList || typeof onChange !== 'function') {
+			console.error(
+				"[react-email-suggestions] - Nothing's returned from rendering. Please provide a baseList and an onChange handler."
+			);
+			return null;
+		}
 
-	return (
-		<div>
-			<input
-				type="email"
-				onChange={(event) => handleInternalChange(event)}
-				value={absoluteValue}
-				defaultValue={defaultValue}
-				ref={internalRef}
-				{...userAttrs}
-			/>
+		return (
+			<div ref={wrapperRef} {...getClasses('wrapperClassName')}>
+				<input
+					ref={(thisElement) => {
+						internalRef.current = thisElement;
+						if (externalRef) {
+							(externalRef as React.MutableRefObject<HTMLInputElement | null>).current =
+								thisElement;
+						}
+					}}
+					onChange={(event) => handleInternalChange(event)}
+					value={absoluteValue}
+					defaultValue={defaultValue}
+					type="email"
+					{...getClasses('inputClassName')}
+					{...userAttrs}
+				/>
 
-			<ul>
-				{isOpen &&
-					internalDomains.map((domain) => (
-						<li key={domain} onClick={(event) => handleSuggestionClick(event)}>
-							{username}@{domain}
+				<ul {...getListStyles()} {...getClasses('dropdownClassName')}>
+					{internalDomains.map((domain) => (
+						<li
+							key={domain}
+							onClick={(event) => handleSuggestionClick(event)}
+							{...getClasses('suggestionClassName')}
+						>
+							<span {...getClasses('usernameClassName')}>{username}</span>
+							<span {...getClasses('domainClassName')}>@{domain}</span>
 						</li>
 					))}
-			</ul>
-		</div>
-	);
-}
+				</ul>
+			</div>
+		);
+	}
+);
+
+Email.displayName = 'Email';
--- vite/App.tsx
+++ vite/App.tsx
@@ -1,17 +1,29 @@
-import { useState } from 'react';
-import { Domain, Email } from '../src/Email';
+import { useRef, useState } from 'react';
+import { Email } from '../src/Email';
 import domainList from '../src/domains.json';
-export const baseList: Domain[] = ['google.com', 'email.com', 'proton.me', 'yahoo.com'];
+
+import '../src/base.css';
+import '../src/input.css';
+
+export const baseList = ['google.com', 'email.com', 'proton.me', 'yahoo.com'];
 
 function App() {
+	const extRef = useRef(null);
+
 	const [email, setEmail] = useState<string | undefined>(undefined);
 
 	console.log(email);
+	console.log(extRef.current);
 
 	return (
 		<div className="App">
 			<Email
+				ref={extRef}
 				placeholder="Ciao"
+				animation="dropdownAnim 300ms ease-out"
+				classNames={{
+					wrapperClassName: 'react-ems customClass',
+				}}
 				value={email}
 				onChange={(value) => setEmail(value)}
 				baseList={baseList}
