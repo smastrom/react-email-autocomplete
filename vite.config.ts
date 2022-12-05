df580763b0f72f5756b70c635fde61b5409a6dd4
“s-mastrom” <60471784+smastrom1@users.noreply.github.com>
2022-12-05 23:36:29 +0100

    add lint staged, edit pkg.json, edit vite.config.ts, delete yarn.lock

--- vite.config.ts
+++ vite.config.ts
@@ -6,39 +6,38 @@ import Package from './package.json';
 export default defineConfig(({ command, mode }) => {
 	if (mode === 'app') {
 		return {
-			plugins: [react()],
+			plugins: [react()]
 		};
 	}
 	return {
 		define: {
-			__DEV__: command !== 'build',
+			__DEV__: command !== 'build'
 		},
 		build: {
 			minify: 'terser',
 			lib: {
 				name: Package.name,
-				entry: 'src/Email.tsx',
+				entry: 'src/index.ts',
 				formats: ['es', 'umd'],
-				fileName: (format) => (format === 'es' ? 'index.js' : 'index.umd.js'),
+				fileName: (format) => (format === 'es' ? 'index.js' : 'index.umd.js')
 			},
 			rollupOptions: {
 				external: ['react', 'react/jsx-runtime'],
-				input: 'src/index.ts',
 				output: {
 					globals: {
 						react: 'React',
-						'react/jsx-runtime': 'React',
-					},
+						'react/jsx-runtime': 'React'
+					}
 				},
 				plugins: [
 					terser({
 						compress: {
-							defaults: true,
-						},
-					}),
-				],
-			},
+							defaults: true
+						}
+					})
+				]
+			}
 		},
-		plugins: [react()],
+		plugins: [react()]
 	};
 });
