ed5033f2895ba700c471b2104d655baaac301fbf
smastrom <smastrom@proton.me>
2023-12-10 01:39:30 +0100

    huge email refactoring
    - format with spaces
    - deprecate `placement` prop
    - add `activeDataAttr` prop
    - add "back-to-first" when keyboard navigating last suggestion
    - add "set-suggestion" when pressing enter and hovering over a suggestion
    - drop custom `uniqueId` in favour of react `useId`
    - drop react 17 support
    - add "use client" directive

--- src/types.ts
+++ src/types.ts
@@ -1,103 +1,86 @@
-import {
-	Dispatch,
-	SetStateAction,
-	FocusEventHandler,
-	KeyboardEventHandler,
-	RefAttributes,
-	ForwardRefExoticComponent,
-	FormEventHandler,
-	ReactNode
-} from 'react';
+import * as React from 'react'
 
-export type Maybe<T> = T | null;
+export type Maybe<T> = T | null
 
 export enum Elements {
-	Wrapper = 'wrapper',
-	Input = 'input',
-	Dropdown = 'dropdown',
-	Suggestion = 'suggestion',
-	Username = 'username',
-	Domain = 'domain'
+   Wrapper = 'wrapper',
+   Input = 'input',
+   Dropdown = 'dropdown',
+   Suggestion = 'suggestion',
+   Username = 'username',
+   Domain = 'domain',
 }
 
-export enum Placement {
-	Top,
-	Bottom
-}
-
-export type ClassNames = Partial<Record<Elements, string>>;
+export type ClassNames = Partial<Record<Elements, string>>
 
 export type OnSelectData = {
-	value: string;
-	keyboard: boolean;
-	position: number;
-};
+   value: string
+   keyboard: boolean
+   position: number
+}
 
-export type Values = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+export type Values = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
 
-export type OnChange =
-	| Dispatch<SetStateAction<string>>
-	| ((newValue: string) => void | Promise<void>);
-export type OnSelect = (object: OnSelectData) => void | Promise<void>;
+export type OnChange = React.Dispatch<React.SetStateAction<string>> | ((newValue: string) => void | Promise<void>)
+export type OnSelect = (object: OnSelectData) => void | Promise<void>
 
 export type Props = {
-	/** State or portion of state to hold the email. */
-	value: string | undefined;
-	/** State setter or custom dispatcher to update the email. */
-	onChange: OnChange;
-	/** Domains to suggest while typing the username. */
-	baseList: string[];
-	/** Domains to refine suggestions after typing @. */
-	refineList?: string[];
-	/** Custom callback to invoke on suggestion select. */
-	onSelect?: OnSelect;
-	/** Minimum chars required to display suggestions. */
-	minChars?: Values;
-	/** Maximum number of suggestions to display. */
-	maxResults?: Omit<Values, 1>;
-	/** Class names for each element. */
-	classNames?: ClassNames;
-	/** Class name of the wrapper element. */
-	className?: string;
-	/** Custom prefix for dropdown unique ID. */
-	customPrefix?: string;
-	/** DOM ID of the wrapper element. */
-	wrapperId?: string;
-	/** Validity state of the field for assistive technologies. */
-	isInvalid?: boolean;
-	/** Dropdown placement. */
-	placement?: 'auto' | 'bottom';
-	children?: ReactNode;
-};
+   /** State or portion of state to hold the email. */
+   value: string | undefined
+   /** State setter or custom dispatcher to update the email. */
+   onChange: OnChange
+   /** Domains to suggest while typing the username. */
+   baseList: string[]
+   /** Domains to refine suggestions after typing @. */
+   refineList?: string[]
+   /** Custom callback to invoke on suggestion select. */
+   onSelect?: OnSelect
+   /** Minimum chars required to display suggestions. */
+   minChars?: Values
+   /** Maximum number of suggestions to display. */
+   maxResults?: Omit<Values, 1>
+   /** Class names for each element. */
+   classNames?: ClassNames
+   /** Class name of the wrapper element. */
+   className?: string
+   /** Custom prefix for dropdown unique ID. */
+   customPrefix?: string
+   /** DOM ID of the wrapper element. */
+   wrapperId?: string
+   /** Validity state of the field for assistive technologies. */
+   isInvalid?: boolean
+   /** Dropdown `aria-label` value */
+   dropdownAriaLabel?: string
+   /** Value of the `data-` attribute to be set on the focuesed/hovered suggestion. Defaults to `data-active-email`. */
+   activeDataAttr?: `data-${string}`
+   children?: React.ReactNode
+   /** Dropdown placement.
+    *
+    * @deprecated Since version 0.9.8 dropdown is always placed below the input.
+    */
+   placement?: 'auto' | 'bottom'
+}
 
 export type Events = {
-	onFocus?: FocusEventHandler<HTMLInputElement>;
-	onBlur?: FocusEventHandler<HTMLInputElement>;
-	onKeyDown?: KeyboardEventHandler<HTMLInputElement>;
-	onInput?: FormEventHandler<HTMLInputElement>;
-};
+   onFocus?: React.FocusEventHandler<HTMLInputElement>
+   onBlur?: React.FocusEventHandler<HTMLInputElement>
+   onKeyDown?: React.KeyboardEventHandler<HTMLInputElement>
+   onInput?: React.FormEventHandler<HTMLInputElement>
+}
 
-export type Attributes = Partial<
-	Pick<
-		HTMLInputElement,
-		'id' | 'placeholder' | 'name' | 'readOnly' | 'required' | 'disabled' | 'pattern'
-	>
->;
-
-export type EmailProps = Props & Events & Attributes;
+export type EmailProps = Props &
+   Events &
+   Partial<
+      Omit<
+         React.HTMLProps<HTMLInputElement>,
+         'onChange' | 'value' | 'onSelect' | 'onFocus' | 'onBlur' | 'onKeyDown' | 'onInput' | 'ref' | 'className'
+      >
+   >
 
 export type LocalizedList = {
-	default: string[];
-} & Record<string, string[]>;
-
-/** Controlled email input component.
- * Read the documentation at: https://github.com/smastrom/react-email-autocomplete. */
-export declare const Email: ForwardRefExoticComponent<EmailProps & RefAttributes<HTMLInputElement>>;
+   default: string[]
+} & Record<string, string[]>
 
 /** List of ~160 world's most popular email providers.
  * Meant to be used with `refineList` prop. */
-export declare const domains: string[];
-
-/** Hook to automatically inject localized lists according to user's browser locale.
- * Read the documentation at: https://github.com/smastrom/react-email-autocomplete. */
-export declare function useLocalizedList(lists: LocalizedList, appLocale?: string): string[];
+export declare const domains: string[]
--- src/utils.ts
+++ src/utils.ts
@@ -1,106 +1,83 @@
-import { useLayoutEffect, useEffect } from 'react';
-
-export const useIsomorphicLayoutEffect =
-	typeof window !== 'undefined' ? useLayoutEffect : useEffect;
-
-export const alphanumericKeys = /^[a-z0-9]$/i;
+export const alphanumericKeys = /^[a-z0-9]$/i
 
 export function cleanValue(value: string) {
-	return value.replace(/\s+/g, '').toLowerCase();
-}
-
-export function getUniqueId() {
-	return (Math.random() + 1).toString(36).substring(7);
+   return value.replace(/\s+/g, '').toLowerCase()
 }
 
 export function getHonestValue(value: unknown, maxValue: number, defaultValue: number) {
-	if (typeof value === 'number' && Number.isInteger(value) && value <= maxValue) {
-		return value;
-	}
-	return defaultValue;
+   if (typeof value === 'number' && Number.isInteger(value) && value <= maxValue) {
+      return value
+   }
+   return defaultValue
 }
 
 export function isFn(fn: unknown) {
-	return typeof fn === 'function';
-}
-
-// Get the first parent that has overflow "auto" or "scroll"
-export function getScrollElement(target: HTMLElement) {
-	if (target.style.position === 'fixed') {
-		return document.documentElement;
-	}
-
-	let hasScroll = false;
-	let nextParent = target.parentElement;
-
-	while (!hasScroll) {
-		if (!nextParent || nextParent === document.documentElement) {
-			break;
-		}
-
-		const { overflow, overflowY } = getComputedStyle(nextParent);
-		hasScroll = /(auto|scroll)/.test(`${overflow}${overflowY}`);
-
-		if (hasScroll) {
-			break;
-		}
-
-		nextParent = nextParent.parentElement;
-	}
-
-	return nextParent;
+   return typeof fn === 'function'
 }
 
 export function getEmailData(value: string, minChars: number) {
-	const [username] = value.split('@');
-	const breakpoint = value.indexOf('@');
-	const domain = breakpoint >= 0 ? value.slice(breakpoint + 1) : ''; // Domain is truthy only if typed @
+   const [username] = value.split('@')
+   const breakpoint = value.indexOf('@')
+   const domain = breakpoint >= 0 ? value.slice(breakpoint + 1) : '' // Domain is truthy only if typed @
 
-	const hasUsername = username.length >= minChars;
-	const hasAt = hasUsername && value.includes('@');
-	const hasDomain = hasUsername && domain.length >= 1;
+   const hasUsername = username.length >= minChars
+   const hasAt = hasUsername && value.includes('@')
+   const hasDomain = hasUsername && domain.length >= 1
 
-	return { username, domain, hasUsername, hasAt, hasDomain };
+   return { username, domain, hasUsername, hasAt, hasDomain }
 }
 
+/**
+ *
+ *
+ *
+ *
+ * getEmailData tests
+ *
+ *
+ *
+ *
+ *
+ */
+
 if (import.meta.vitest) {
-	const { it, expect, describe } = import.meta.vitest;
+   const { it, expect, describe } = import.meta.vitest
 
-	describe('Domain', () => {
-		it('Should return domain', () => {
-			const { domain } = getEmailData('username@gmail.com', 2);
-			expect(domain).toBe('gmail.com');
+   describe('Domain', () => {
+      it('Should return domain', () => {
+         const { domain } = getEmailData('username@gmail.com', 2)
+         expect(domain).toBe('gmail.com')
 
-			const { domain: domain2 } = getEmailData('username@g', 2);
-			expect(domain2).toBe('g');
-		});
+         const { domain: domain2 } = getEmailData('username@g', 2)
+         expect(domain2).toBe('g')
+      })
 
-		it('Should return domain even if more @', () => {
-			const { domain } = getEmailData('username@ciao@', 2);
-			expect(domain).toBe('ciao@');
-		});
+      it('Should return domain even if more @', () => {
+         const { domain } = getEmailData('username@ciao@', 2)
+         expect(domain).toBe('ciao@')
+      })
 
-		it('Should return empty string if no domain', () => {
-			const { domain } = getEmailData('username@', 2);
-			expect(domain).toBe('');
+      it('Should return empty string if no domain', () => {
+         const { domain } = getEmailData('username@', 2)
+         expect(domain).toBe('')
 
-			const { domain: domain2 } = getEmailData('username', 2);
-			expect(domain2).toBe('');
-		});
-	});
+         const { domain: domain2 } = getEmailData('username', 2)
+         expect(domain2).toBe('')
+      })
+   })
 
-	describe('hasDomain', () => {
-		it('Should return true', () => {
-			const { hasDomain } = getEmailData('username@gmail.com', 2);
-			expect(hasDomain).toBe(true);
-		});
+   describe('hasDomain', () => {
+      it('Should return true', () => {
+         const { hasDomain } = getEmailData('username@gmail.com', 2)
+         expect(hasDomain).toBe(true)
+      })
 
-		it('Should return false', () => {
-			const { hasDomain } = getEmailData('username@', 2);
-			expect(hasDomain).toBe(false);
+      it('Should return false', () => {
+         const { hasDomain } = getEmailData('username@', 2)
+         expect(hasDomain).toBe(false)
 
-			const { hasDomain: hasDomain2 } = getEmailData('username', 2);
-			expect(hasDomain2).toBe(false);
-		});
-	});
+         const { hasDomain: hasDomain2 } = getEmailData('username', 2)
+         expect(hasDomain2).toBe(false)
+      })
+   })
 }
